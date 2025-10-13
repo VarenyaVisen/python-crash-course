c937bc941a6f025d3c2799f9ea139e36ab3672dd
Varenya Visen <varenyavisen@gmail.com>
2025-10-13 11:22:10 +0530

    using tab and new line strings

---
+++ strings/more_methods.py
@@ -0,0 +1,8 @@
+# adding tabs in String
+print("Python")
+print("\tPython")
+
+# Adding a new line in String
+print("Languages:\nPython\nC\nJavaScript\nJava\nC++")
+# Adding new line and tab at once
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
