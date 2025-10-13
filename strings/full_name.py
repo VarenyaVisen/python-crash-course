a5b53fc256fd56de41dab9f4206215d45d5c3331
Varenya Visen <varenyavisen@gmail.com>
2025-10-13 11:18:46 +0530

    f string in python

--- strings/full_name.py
+++ strings/full_name.py
@@ -3,3 +3,9 @@ last_name = "lovelace"
 full_namee = f"{first_name} {last_name}"
 
 print(full_namee)
+
+print(f"Hello, {full_namee.title()}!")
+
+#We can aslo compose a message using f string and assign it to a variable
+message = f"Welcome to the team : {full_namee.title()}!!"
+print(message)
