470121885f44f294e97942cd3f08ad74fad9b046
Varenya Visen <varenyavisen@gmail.com>
2026-01-03 22:52:46 +0530

    using pop() with specified loaction of an item to remove

--- list/motorcycles3.py
+++ list/motorcycles3.py
@@ -1,3 +1,7 @@
 motorcycles = ['honda','yamaha','suzuki']
 last_owned = motorcycles.pop()
 print(f"The last motorcycle I owned was {last_owned.title()}.")
+
+# popping item from any position 
+first_owned = motorcycles.pop(0)
+print(f"The first motorcycle I owned was {first_owned.title()}.")
