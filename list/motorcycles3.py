73d6f96209e2d414c504694fa80b893d775c2bfb
Varenya Visen <varenyavisen@gmail.com>
2026-01-04 20:12:55 +0530

    remove() function when dont know the position of the element

--- list/motorcycles3.py
+++ list/motorcycles3.py
@@ -5,3 +5,8 @@ print(f"The last motorcycle I owned was {last_owned.title()}.")
 # popping item from any position 
 first_owned = motorcycles.pop(0)
 print(f"The first motorcycle I owned was {first_owned.title()}.")
+
+# removing item when we dont know the position of the element
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+motorcycles.remove('ducati') 
+print(motorcycles)
