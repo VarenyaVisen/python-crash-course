c440c277fea4ee7633ffb19cbc8a096983629eac
Varenya Visen <varenyavisen@gmail.com>
2026-02-01 20:50:30 +0530

    exercise 3.2- inviting guest using list

---
+++ Exercise/ex3.2.py
@@ -0,0 +1,5 @@
+guests = ["Albert Einstein", "APJ Abdul Kalam", "Steve Jobs"]
+
+print("Dear " + guests[0] + ", I would be honored to invite you to dinner.")
+print("Dear " + guests[1] + ", I would be honored to invite you to dinner.")
+print("Dear " + guests[2] + ", I would be honored to invite you to dinner.")
