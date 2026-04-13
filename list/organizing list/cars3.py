a3762d01acb14c2b9ca7e07b1df3b13572fa4a3d
Varenya Visen <varenyavisen@gmail.com>
2026-04-13 09:58:33 +0530

    sorted() function in list

---
+++ list/organizing list/cars3.py
@@ -0,0 +1,5 @@
+cars = ['bmw','audi', 'toyota', 'subaru']
+
+print("Here is the sorted list : {}".format(sorted(cars)))
+
+print("here is the original list : \n{}".format(cars))
---
+++ list/working with list/loops/magicians.py
@@ -0,0 +1,4 @@
+# Using for loop to access the whole list 
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
