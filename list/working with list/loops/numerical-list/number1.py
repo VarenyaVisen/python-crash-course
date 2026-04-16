32da1b85a5e106199096e911ddf1cbdb54875ca2
Varenya Visen <varenyavisen@gmail.com>
2026-04-16 12:19:38 +0530

    make number list using range function

---
+++ list/working with list/loops/numerical-list/number1.py
@@ -0,0 +1,11 @@
+for i in range(1,5):
+    print(i)
+
+
+# we can also make list using list()
+numbers =  list(range(1,6))
+print(numbers)
+
+# we can also tell python to skip some numbers in range() function
+num = list(range(0,11,2))
+print(num)
---
+++ list/working with list/loops/numerical-list/squares_number.py
@@ -0,0 +1,13 @@
+squares = []
+for value in range(1,11):
+    square = value**2
+    squares.append(square)
+
+print(squares)
+
+
+# we can write it more concisely
+sq = []
+for value in range(1,11):
+    sq.append(value**2)
+print(sq)
