8afde69ec5d435d6c2968f3b95d2d5d38b54c9e9
Varenya Visen <varenyavisen@gmail.com>
2026-04-16 10:30:33 +0530

    indentation error in for loop

---
+++ list/working with list/loops/magician3.py
@@ -0,0 +1,6 @@
+
+# Avoid indenatation error
+magicians = ['alice','david','carolina']
+for magician in magicians:
+    print(f"{magician.title()} that was a great trick")
+print(f"We cant wait to see your next trick, {magician.title()}")
