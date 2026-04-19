41e95cf1cc9acf6c6a2392984be602656cd69a9c
Varenya Visen <varenyavisen@gmail.com>
2026-04-19 17:46:14 +0530

    ensuring 2 separate lists were created

--- list/copy_list/foods.py
+++ list/copy_list/foods.py
@@ -4,3 +4,15 @@ friend_foods = my_foods[:]
 print("My favorite foods are: {}".format(my_foods))
 
 print("My friend favourite foods are: {}".format(friend_foods))
+
+my_foods.append('ice cream')
+friend_foods.append('cannoli')
+
+print("My favorite foods are: {}".format(my_foods))
+
+print("My friend favourite foods are: {}".format(friend_foods))
+
+
+a = ['a','b','c']
+b = a
+print(a)
