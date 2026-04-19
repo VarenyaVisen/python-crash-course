c94f835cd97151fdc9caf007565fe45356856f8e
Varenya Visen <varenyavisen@gmail.com>
2026-04-19 17:24:04 +0530

    slicing with end and start

--- list/slicing_list/players.py
+++ list/slicing_list/players.py
@@ -1,3 +1,9 @@
 # Slicing in python lets you print or take the part of a list it helps you create a subset of list
 players = ['charles', ' martina', 'florence','eli']
 print(players[0:3])
+
+# if you didn't write the index in end python automatically goes till the end
+print(players[2:])
+
+# same if not written in starting python starts with first element
+print(players[:2])
