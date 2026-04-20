d6b5b33fc82b58e0f822af904b7e57e2ec1b9638
Varenya Visen <varenyavisen@gmail.com>
2026-04-20 09:30:48 +0530

    method [:] to make an actual copy of list

--- list/copy_list/foods.py
+++ list/copy_list/foods.py
@@ -12,7 +12,10 @@ print("My favorite foods are: {}".format(my_foods))
 
 print("My friend favourite foods are: {}".format(friend_foods))
 
-
+# Thus method only assings the list to another variable it does not actually make a copy
+# we cant work on both list separately
 a = ['a','b','c']
 b = a
-print(a)
+print(b)
+b.append('z')
+print(a) # see i appended the value in list b but a automatically updated because they are pointing the same list
