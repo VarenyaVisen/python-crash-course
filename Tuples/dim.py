66deab7792ebcd1891837ef747766c7c770c712c
Varenya Visen <varenyavisen@gmail.com>
2026-04-20 10:04:16 +0530

    reassigning the tuplle instead of changing it

---
+++ Tuples/dim.py
@@ -0,0 +1,10 @@
+# we cannot change the tuple but we can reassign them
+dimensions = (200, 50)
+print("Orifinal dimensions : ")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 100)
+print("Modified dimensions : ")
+for dimension in dimensions:
+    print(dimension)
