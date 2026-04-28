55de3418010d83e8e88161acec8a5bce1a103bf4
Varenya Visen <varenyavisen@gmail.com>
2026-04-28 11:48:10 +0530

    Did an exercise on tuple and leanred that you can change tuples

---
+++ Tuples/restaurant.py
@@ -0,0 +1,8 @@
+buffet = ("Butter chicken", "matar paneer", "shahi paneer", "malai chaap", "rajma chawal")
+for food in buffet:
+    print(f"The restaurant offers: {food}")
+
+buffet = (("Butter chicken", "matar paneer", "shahi paneer", "malai chaap", "afghani chaap"))
+
+for food in buffet:
+    print(f"The restaurant offers: {food}")
