6af8ef40ba8b6e477afd5cb444503035bb4461f6
Varenya Visen <varenyavisen@gmail.com>
2026-05-11 13:52:12 +0530

    numerical and mathematical comparison

---
+++ condition/magic_number.py
@@ -0,0 +1,8 @@
+# This is a numerical comparison
+answer = 17
+if answer != 42:
+    print("That is not the correct answer. Please try again!")
+
+# We can do mathematical comparison as well
+if answer < 21:
+    print("The answer is less than 21")
