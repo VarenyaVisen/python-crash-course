a1f63c6d464d451120502c7635ea6f0a08f95342
Varenya Visen <varenyavisen@gmail.com>
2026-05-11 13:55:41 +0530

    checking value present in list or not

---
+++ condition/banned_users.py
@@ -0,0 +1,9 @@
+banned_users = ['andrew', 'carolina', 'david']
+user_1 = 'marie'
+user_2 = 'andrew'
+
+if user_1 not in banned_users:
+    print("You are welcome!")
+
+if user_2 in banned_users:
+    print("Sorry you are not allowed to use this platform!")
