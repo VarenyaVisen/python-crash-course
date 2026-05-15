d74c81544410129244cbbe1e11a7e8e0df12c76f
Varenya Visen <varenyavisen@gmail.com>
2026-05-15 10:40:16 +0530

    testing multiple conditions

---
+++ condition/toppings2.py
@@ -0,0 +1,12 @@
+requested_toppings = ['mushroom','extra cheese','pepperoni']
+
+if 'mushroom' in requested_toppings:
+    print(f"Adding {requested_toppings[0]}")
+if 'pepperoni' in requested_toppings:
+    print(f"Adding {requested_toppings[2]}")
+if 'extra cheese' in requested_toppings:
+    print(f"Addin {requested_toppings[1]}")
+
+print("\n Finished making your pizza")
+
+#Note -  when using elif statement if one condition is true the python will not check the below statements it will ignore
