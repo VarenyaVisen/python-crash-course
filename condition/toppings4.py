a9b4def2b8757f954836db1e3e7c98d1bbb907da
Varenya Visen <varenyavisen@gmail.com>
2026-05-18 10:01:14 +0530

    checking if the list is empty or not

---
+++ condition/toppings4.py
@@ -0,0 +1,7 @@
+requested_toppings = []
+
+if requested_toppings:
+    for toppnigs in requested_toppings:
+        print(f"Adding- {toppnigs}")
+else:
+    print("Are you sure you want a plain pizza?")
