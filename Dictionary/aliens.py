d64b7775ead1fdcb12703735b1ea720e4d3a5cef
Varenya Visen <varenyavisen@gmail.com>
2026-06-03 11:12:19 +0530

    dictionary a key value pair based data structure and how to access value using key

---
+++ Dictionary/aliens.py
@@ -0,0 +1,9 @@
+# Dictionary - A dictionary in python is collection of key value pairs where each key is connected to a value
+
+alien_0 = {"color": "green", "points": 5}
+
+print(alien_0['color']) # this is how we access the value associated with the key
+print(alien_0['points'])
+
+new_points = alien_0['points']
+print(f"You just earned {new_points} point...")
