59d38841256924f24ffc8d609589f7e7dcbed8ef
Varenya Visen <varenyavisen@gmail.com>
2026-06-22 10:24:39 +0530

    modifying values in dictionary

--- Dictionary/alien3.py
+++ Dictionary/alien3.py
@@ -5,3 +5,9 @@ alien_0['Points'] = 5
 
 print(alien_0)
 
+# we can also modify values in Dictionary
+alien_0 = {'Color':'Green'}
+print(f"The alien is {alien_0['Color']}.")
+
+alien_0['Color'] = 'Yellow'
+print(f"The alien is {alien_0['Color']}.")
