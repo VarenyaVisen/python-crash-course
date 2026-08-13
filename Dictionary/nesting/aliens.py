8b86f4e9aeadf3c07ab35f22375f15f765e26905
Varenya Visen <varenyavisen@gmail.com>
2026-08-13 12:20:45 +0530

    nested dict - dictionary inside a list

---
+++ Dictionary/nesting/aliens.py
@@ -0,0 +1,8 @@
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'yellow', 'points': 15}
+alien_2 = {'color': 'red', 'points': 30}
+
+aliens = [alien_0, alien_1, alien_2]
+
+for alien in aliens:
+    print(alien)
---
+++ Dictionary/nesting/aliens2.py
@@ -0,0 +1,20 @@
+aliens = [] # an empty list
+
+# Make 30 aliens
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+for alien in aliens[:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+
+# show firs five aliens
+for alien in aliens[:5]:
+    print(alien)
+print('...')
+
+# show number of alien
+print(f"Total number of aliens: {len(aliens)}")
