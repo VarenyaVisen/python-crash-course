911b695cab52185387cf91f1cc52733f59e830e7
Varenya Visen <varenyavisen@gmail.com>
2026-09-25 13:14:42 +0530

    Method override in python

---
+++ OOPS/Inheritance/car3.py
@@ -0,0 +1,64 @@
+class Car:
+    """A simple attempt to represent a car."""
+
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car"""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_description(self):
+        """Return a neatly formatted descriptive name"""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometer(self):
+        """Print a statement showing the car's milage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage):
+        """Set the odometer reading to the given value."""
+        self.odometer_reading = mileage
+
+    def fill_gas_tank(self,gas):
+        """The amount of gas can be filled in the tank"""
+        self.gas = gas
+        print(f"The amount of gas that can be filled in the tank is : {self.gas} Litres")
+
+
+my_new_car = Car('audi', 'a4', 2024)
+
+print(my_new_car.get_description())
+my_new_car.read_odometer()
+
+my_new_car.odometer_reading = 23 # Modifying attribute directly
+my_new_car.read_odometer()
+
+my_new_car.update_odometer(34)  # Modifying value using a function
+my_new_car.read_odometer()
+
+my_new_car.fill_gas_tank(50)
+# You dont have to start from scratch when writing a class
+# If the class you're writing is a specialized version of another class already written 
+# Then we can use Inheritance
+class ElectricCar(Car):
+    """Represent aspects of car, specific to electric vehicles. """
+
+    def __init__(self,make , model, year):
+        """Initialize attributes of Parent class"""
+        super().__init__(make,model,year) # The super function is a special function that allows you to call a method from the parent class
+        self.battery_size = 40
+
+    def battery_description(self):
+        """Print a statement describing the battery size"""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def fill_gas_tank(self):        # This is method override - defining a method in the child class with the same name as the parent class
+        """Electric cars don't have gas tanks."""
+        print("This car does'nt have a gas tank!")
+
+my_leaf = ElectricCar('nissan','leaf',2024)
+print(my_leaf.get_description())
+my_leaf.battery_description()
+my_leaf.fill_gas_tank()
